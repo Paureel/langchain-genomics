3706a1f431461d5c0d57f303c26e4b434ed73b23
Aurél Prósz <krs118@alumni.ku.dk>
2023-01-30 20:54:39 -0500

    Update twitter.py

--- langchain/utilities/twitter.py
+++ langchain/utilities/twitter.py
@@ -64,75 +64,9 @@ class TWITTERAPIWrapper(BaseModel):
      
      
     # function to perform data extraction
-    def scrape(words, date_since, numtweet):
-            alltweets = {}
-            # Creating DataFrame using pandas
-            db = pd.DataFrame(columns=['username',
-                                       'description',
-                                       'location',
-                                       'following',
-                                       'followers',
-                                       'totaltweets',
-                                       'retweetcount',
-                                       'text',
-                                       'hashtags'])
-     
-            # We are using .Cursor() to search
-            # through twitter for the required tweets.
-            # The number of tweets can be
-            # restricted using .items(number of tweets)
-            tweets = tweepy.Cursor(api.search,
-                                   words, lang="en",
-                                   since_id=date_since,
-                                   tweet_mode='extended').items(numtweet)
-     
-     
-            # .Cursor() returns an iterable object. Each item in
-            # the iterator has various attributes
-            # that you can access to
-            # get information about each tweet
-            list_tweets = [tweet for tweet in tweets]
-     
-            # Counter to maintain Tweet Count
-            i = 1
-     
-            # we will iterate over each tweet in the
-            # list for extracting information about each tweet
-            for tweet in list_tweets:
-                    username = tweet.user.screen_name
-                    description = tweet.user.description
-                    location = tweet.user.location
-                    following = tweet.user.friends_count
-                    followers = tweet.user.followers_count
-                    totaltweets = tweet.user.statuses_count
-                    retweetcount = tweet.retweet_count
-                    hashtags = tweet.entities['hashtags']
-     
-                    # Retweets can be distinguished by
-                    # a retweeted_status attribute,
-                    # in case it is an invalid reference,
-                    # except block will be executed
-                    try:
-                            text = tweet.retweeted_status.full_text
-                    except AttributeError:
-                            text = tweet.full_text
-                    hashtext = list()
-                    for j in range(0, len(hashtags)):
-                            hashtext.append(hashtags[j]['text'])
-     
-                    # Here we are appending all the
-                    # extracted information in the DataFrame
-                    ith_tweet = [username, description,
-                                 location, following,
-                                 followers, totaltweets,
-                                 retweetcount, text, hashtext]
-                    db.loc[len(db)] = ith_tweet
-     
-                    # Function call to print tweet data on screen
-                    
-                    alltweets[i] = ith_tweet[7]
-                    i = i+1
-            return(alltweets)
+    
+            
+            
 
     def run(self, query: str) -> str:
         """Run query through Twitter and parse result."""
@@ -149,8 +83,80 @@ class TWITTERAPIWrapper(BaseModel):
         auth.set_access_token(access_key, access_secret)
         api = tweepy.API(auth)
         
+        words = query
+        date_since ="2023-01--20", 
+        numtweet =  50
+        alltweets = {}
+            # Creating DataFrame using pandas
+        db = pd.DataFrame(columns=['username',
+                                   'description',
+                                   'location',
+                                   'following',
+                                   'followers',
+                                   'totaltweets',
+                                   'retweetcount',
+                                   'text',
+                                   'hashtags'])
+ 
+        # We are using .Cursor() to search
+        # through twitter for the required tweets.
+        # The number of tweets can be
+        # restricted using .items(number of tweets)
+        tweets = tweepy.Cursor(api.search,
+                               words, lang="en",
+                               since_id=date_since,
+                               tweet_mode='extended').items(numtweet)
+ 
+ 
+        # .Cursor() returns an iterable object. Each item in
+        # the iterator has various attributes
+        # that you can access to
+        # get information about each tweet
+        list_tweets = [tweet for tweet in tweets]
+ 
+        # Counter to maintain Tweet Count
+        i = 1
+ 
+        # we will iterate over each tweet in the
+        # list for extracting information about each tweet
+        for tweet in list_tweets:
+                username = tweet.user.screen_name
+                description = tweet.user.description
+                location = tweet.user.location
+                following = tweet.user.friends_count
+                followers = tweet.user.followers_count
+                totaltweets = tweet.user.statuses_count
+                retweetcount = tweet.retweet_count
+                hashtags = tweet.entities['hashtags']
+ 
+                # Retweets can be distinguished by
+                # a retweeted_status attribute,
+                # in case it is an invalid reference,
+                # except block will be executed
+                try:
+                        text = tweet.retweeted_status.full_text
+                except AttributeError:
+                        text = tweet.full_text
+                hashtext = list()
+                for j in range(0, len(hashtags)):
+                        hashtext.append(hashtags[j]['text'])
+ 
+                # Here we are appending all the
+                # extracted information in the DataFrame
+                ith_tweet = [username, description,
+                             location, following,
+                             followers, totaltweets,
+                             retweetcount, text, hashtext]
+                db.loc[len(db)] = ith_tweet
+ 
+                # Function call to print tweet data on screen
+                
+                alltweets[i] = ith_tweet[7]
+                i = i+1
+        
             
-        answer = self.scrape(query, "2023-01--20", 50)
+        answer = alltweets
+     
 
         
             
